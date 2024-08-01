390f226a8b1f37e33a81f159adf6de7d373cb844
downtothemeyer <jmeyer1220@gmail.com>
2024-07-31 20:53:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </div>
 
 <div class="relative h-32 w-32">
-<div class="absolute inset-y-0 right-0 w-16"><h1 className="text-6xl  font-bold mb-4">ARCHETYPE/h1></div>
+<div class="absolute inset-y-0 right-0 w-16"><h1 className="text-6xl  font-bold mb-4">ARCHETYPE</h1></div>
 </div>
       <ArchetypeQuiz />
     </div>
