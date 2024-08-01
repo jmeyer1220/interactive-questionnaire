19031e13996d37761c442b874d3595f7f757aacb
downtothemeyer <jmeyer1220@gmail.com>
2024-07-31 20:52:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4 py-72 max-h-dvh">
 
-      <!-- Span left edge -->
-      <div class="relative h-32 w-32 ...">
-        <div class="absolute inset-y-0 left-0 w-16 ..."><h1 className="text-6xl font-bold mb-4">BRAND</h1></div>
+      <div class="relative h-32 w-32">
+        <div class="absolute inset-y-0 left-0 w-16"><h1 className="text-6xl font-bold mb-4">BRAND</h1></div>
       </div>
 
 <div class="relative h-32 w-32">
