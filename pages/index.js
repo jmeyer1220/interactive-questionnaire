d56c2322bbd6b78a959dee0faa207add294f8841
downtothemeyer <jmeyer1220@gmail.com>
2024-07-31 21:06:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="container mx-auto px-4 py-72 relative">
       <div class="absolute inset-y-0 left-0 w-16"><h1 className="text-[300px] font-bold opacity-15 mb-4 rotate-90">BRAND</h1></div>
       <ArchetypeQuiz />
-      <div class="absolute inset-y-0 right-0 w-16"><h1 className="text-[300px]  opacity-15font-bold mb-4 rotate-90">ARCHETYPE</h1></div>
+      <div class="absolute inset-y-0 right-0 w-16"><h1 className="text-[300px]  opacity-15 font-bold mb-4 -rotate-90">ARCHETYPE</h1></div>
 
     </div>
   );
