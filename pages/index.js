97dd63002575dca378bfd7b6a62a12226bc390be
downtothemeyer <jmeyer1220@gmail.com>
2024-07-31 21:05:35 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import ArchetypeQuiz from "../components/ArchetypeQuiz";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 py-72 max-h-dvh relative">
-      <div class="absolute inset-y-0 left-0 w-16"><h1 className="text-[300px] font-bold mb-4 rotate-90">BRAND</h1></div>
-            <h1 className="text-6xl font-bold mb-4">Archetypes</h1>
+    <div className="container mx-auto px-4 py-72 relative">
+      <div class="absolute inset-y-0 left-0 w-16"><h1 className="text-[300px] font-bold opacity-15 mb-4 rotate-90">BRAND</h1></div>
       <ArchetypeQuiz />
-      <div class="absolute inset-y-0 right-0 w-16"><h1 className="text-[300px]  font-bold mb-4 rotate-90">ARCHETYPE</h1></div>
+      <div class="absolute inset-y-0 right-0 w-16"><h1 className="text-[300px]  opacity-15font-bold mb-4 rotate-90">ARCHETYPE</h1></div>
 
     </div>
   );
